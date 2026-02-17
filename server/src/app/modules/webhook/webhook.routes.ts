d7191b6d28a0bcdca95fd8aa45bdd2c5aa3fc8bf
Agnik Saha <agniksaha9953@gmail.com>
2026-02-17 11:42:58 +0530

    l1

--- server/src/app/modules/webhook/webhook.routes.ts
+++ server/src/app/modules/webhook/webhook.routes.ts
@@ -7,7 +7,8 @@ import { body, validationResult } from "express-validator";
 const validate = (req: Request, res: Response, next: NextFunction) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(400).json({ success: false, errors: errors.array() });
+    res.status(400).json({ success: false, errors: errors.array() });
+    return;
   }
   next();
 };
